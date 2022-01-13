48ecec010b9785f778fe15f2439639dd7688702f
mihaitranda94 <mihaitranda94@gmail.com>
2022-01-13 15:19:01 -0800

    db script

---
+++ db/init-db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    email varchar NOT NULL,
+    phone varchar NOT NULL
+);
