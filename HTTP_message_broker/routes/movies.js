78f3e80cc6cf9833c067169b273ba9aae6cf494b
mihaitranda94 <mihaitranda94@gmail.com>
2022-01-16 01:03:52 -0800

    small update with cors

--- HTTP_message_broker/routes/movies.js
+++ HTTP_message_broker/routes/movies.js
@@ -6,7 +6,7 @@ Router.get("/trending", async (req, res) => {
     console.info(`Forwarding request for get trending movies ...`);
 
     const getTrendingMovies = {
-        url: 'http://localhost:5001/api/movies/trending',
+        url: `http://${process.env.HOST}:4002/api/movies/trending`,
     };
     console.log(process.env.MOVIE_PROCESSING_ROUTE)
     const trending = await sendRequest(getTrendingMovies);
@@ -18,7 +18,7 @@ Router.get("/topRatedRecommended", async (req, res) => {
     console.info(`Forwarding request for getting top rated movie recommendations ...`);
 
     const getTopRated= {
-        url: `http://127.0.0.1:5001/${process.env.MOVIE_PROCESSING_ROUTE}/topRatedRecommended`,
+        url: `http://${process.env.HOST}:4002/${process.env.MOVIE_PROCESSING_ROUTE}/topRatedRecommended`,
     };
 
     const topRatedResponse = await sendRequest(getTopRated);
@@ -33,7 +33,7 @@ Router.get("/movieDetails/:tmdbId", async (req, res) => {
     console.info(`Forwarding request for get movie details by id ${id} ...`);
 
     const getMovieDetailsTmdbIdRequest = {
-        url: `http://localhost:5001/${process.env.MOVIE_PROCESSING_ROUTE}/movieDetails/${id}`,
+        url: `http://${process.env.HOST}:4002/${process.env.MOVIE_PROCESSING_ROUTE}/movieDetails/${id}`,
     };
     console.error(getMovieDetailsTmdbIdRequest.url)
     const MovieDetails = await sendRequest(getMovieDetailsTmdbIdRequest);
@@ -47,7 +47,7 @@ Router.get("/movieAutocomplete", async (req, res) => {
     const query = req.query.q
 
     const getAutocomplete = {
-        url: `http://localhost:5001/${process.env.MOVIE_PROCESSING_ROUTE}/movieAutocomplete?q=${query}`,
+        url: `http://${process.env.HOST}:4002/${process.env.MOVIE_PROCESSING_ROUTE}/movieAutocomplete?q=${query}`,
     };
 
     const autocompleteResponse = await sendRequest(getAutocomplete);
